76d9c6d521252aeb5bf45416de0e4c72f9acde17
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-03-13 12:16:21 +0500

    fix js

--- landing/index.js
+++ landing/index.js
@@ -2,6 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const slides = document.querySelectorAll(".slide");
   let currentSlide = 0;
 
+  slides.forEach((slide, index) => {
+    if (index === currentSlide) {
+      slide.style.display = "flex";
+    } else {
+      slide.style.display = "none";
+    }
+  });
+
   setInterval(() => {
     currentSlide = (currentSlide + 1) % slides.length;
     slides.forEach((slide, index) => {
