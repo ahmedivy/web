5dbc80a004fcef4adc6aed11dc5f60268570d384
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-03-16 12:34:32 +0500

    created server for assignment

--- ajax-crud/api/index.js
+++ ajax-crud/api/index.js
@@ -1,7 +1,69 @@
-const express = require("express");
+import cors from "cors";
+import express from "express";
+import mongoose, { Schema, model } from "mongoose";
 
 const app = express();
-app.get("/", (req, res) => res.send("Express on Vercel"));
-app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas!");
+  })
+  .catch((e) => {
+    console.log(`Unable to connect to MongoDB.\nError: ${e}`);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+const Project = model(
+  "project",
+  new Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/projects", async (req, res) => {
+  const projects = await Project.find();
+  res.json(projects);
+});
+
+app.post("/projects", async (req, res) => {
+  console.log(req.body);
+
+  const project = new Project(req.body);
+  await project.save();
+  res.json(project);
+});
+
+app.put("/projects/:id", async (req, res) => {
+  const project = await Project.findById(req.params.id);
+  project.set(req.body);
+  await project.save();
+  res.json(project);
+});
+
+app.get("/projects/:id", async (req, res) => {
+  const project = await Project.findById(req.params.id);
+  res.json(project);
+});
+
+app.delete("/projects/:id", async (req, res) => {
+  await Project.deleteOne({ _id: req.params.id });
+  res.json({ message: "Project deleted" });
+});
+
+app.listen(3000, (req, res) => {
+  console.log("Server Initialized!");
+});
